8b6c0fd45782bb25da881ba67f006afdfc6e9bc5
Love2Bit <aodellas1434@gmail.com>
2024-06-01 15:20:12 +0700

    Refactor search.php to improve error handling and redirect logic

--- search.php
+++ search.php
@@ -1,58 +1,86 @@
 <?php
-  include 'conf/connection.php';
-  session_start();
-  if(!isset($_SESSION['email'])) {
-      echo "<script>
-          alert('You must login first!!');
-          window.location.href='index.php';
+include 'conf/connection.php';
+session_start();
+
+if (!isset($_SESSION['email'])) {
+    echo "<script>
+            alert('You must login first!');
+            window.location.href='index.php';
           </script>";
-  }
+    exit(); 
+}
 
-  if (isset($_POST['search'])) {
-    $search_term = mysqli_real_escape_string($conn, trim($_POST['search'])); 
+if (isset($_POST['search'])) {
+    $search_term = mysqli_real_escape_string($conn, trim($_POST['search']));
 
-    if (preg_match("/^[a-zA-Z]+$/", $search_term)) { 
-      $search_month = date('m', strtotime($search_term)); 
-      $search_query = "SELECT * FROM daftarkonser 
-      WHERE nama_konser LIKE ? 
-      OR MONTH(tgl_konser) = ? 
-      OR kota LIKE ?";
-      $stmt = mysqli_prepare($conn, $search_query);
-      mysqli_stmt_bind_param($stmt, "sss", $search_term, $search_month, $search_term); // Pass variables directly
+    
+    if (preg_match("/^[a-zA-Z]+$/", $search_term)) {
+        $search_month = date('m', strtotime($search_term)); 
+        $search_query = "SELECT * FROM daftarkonser
+                          WHERE nama_konser LIKE ?
+                          OR MONTH(tgl_konser) = ?"; 
 
-    } else {
-      
-      $search_date_parts = explode(" ", $search_term);
+        if (strlen($search_term) > 2) { 
+            $search_query .= " OR kota LIKE ?"; 
+        }
 
-      if (count($search_date_parts) === 2) {
-        $search_day = (int) $search_date_parts[0];
-        $search_month = date('m', strtotime($search_date_parts[1])); 
-
-        $search_query = "SELECT * FROM daftarkonser 
-                          WHERE DAY(tgl_konser) = ? 
-                          AND MONTH(tgl_konser) = ?";
         $stmt = mysqli_prepare($conn, $search_query);
-        mysqli_stmt_bind_param($stmt, "ii", $search_day, $search_month); 
-      } else {
-        
-        echo "<script>
-        alert('Invalid search format. Please enter either concert name, month name (e.g., June), or date as day month (e.g., 26 June).');
-        </script>";
-        header("Location: home.php");
-        exit();
-      }
+        $bind_params = [];
+        $bind_params_type = "";
+
+        $bind_params[] = "%" . $search_term . "%";
+        $bind_params_type .= "s";
+
+        if ($search_month > 0) { 
+            $bind_params[] = $search_month;
+            $bind_params_type .= "i";
+        }
+
+        if (strlen($search_term) > 2) { 
+            $bind_params[] = "%" . $search_term . "%";
+            $bind_params_type .= "s";
+        }
+
+        mysqli_stmt_bind_param($stmt, $bind_params_type, ...$bind_params);
+
+    } else { 
+        $search_date_parts = explode(" ", $search_term);
+
+        if (count($search_date_parts) === 2) {
+            $search_day = (int) $search_date_parts[0];
+            $search_month = date('m', strtotime($search_date_parts[1]));
+
+            $search_query = "SELECT * FROM daftarkonser
+                              WHERE DAY(tgl_konser) = ?
+                              AND MONTH(tgl_konser) = ?";
+            $stmt = mysqli_prepare($conn, $search_query);
+            mysqli_stmt_bind_param($stmt, "ii", $search_day, $search_month);
+        } else {
+            echo "<script>
+                    alert('Invalid search format. Please enter either concert name, month name (e.g., June), or date as day month (e.g., 26 June).');
+                  </script>";
+            header("Location: home.php");
+            exit();
+        }
     }
 
-    mysqli_stmt_execute($stmt); 
-    $search_result = mysqli_stmt_get_result($stmt); 
-  } else {
+    mysqli_stmt_execute($stmt);
+    $search_result = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($search_result) === 0) {
+        echo "<script>
+                alert('No concerts found for your search term.');
+              </script>";
+    }
+} else {
     
     $query = "SELECT * FROM daftarkonser";
     $result = mysqli_query($conn, $query);
-  }
+}
 
-  mysqli_close($conn); 
+mysqli_close($conn);
 ?>
+
 
 
 <!DOCTYPE html>
