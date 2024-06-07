d18e3317c9a5a6576d4a67364fde5d610928916e
hera tony <hera.chantica@ti.ukdw.ac.id>
2024-06-07 19:18:26 +0700

    halaman pemesanan done
    pemesanan sudah bisa masuk ke databasse invoice, yang kurang hanya bagian invoice tok

--- riwayat.php
+++ riwayat.php
@@ -56,17 +56,49 @@ try {
             $errorMessage = "Sorry, the selected quantity exceeds the available stock for this ticket type.";
         } else {
             // If quantity is within available stock, proceed with booking
-            // Your booking logic here
-            $errorMessage = ""; // Clear the error message if booking is successful
+            // Insert booking details into the invoice table
+            $namaPemesan = $_POST['nama_pemesan'];
+            $noTlpPemesan = $_POST['tlp_pemesan'];
+            $emailPemesan = $_POST['email_pemesan'];
+            $datakonserId = $concertId;
+            $harga = 0;
+            foreach ($ticketTypes as $ticket) {
+                if ($ticket['jenis'] === $selectedTicket) {
+                    $harga = $ticket['harga'];
+                    break;
+                }
+            }
+            $totalHarga = $harga * $selectedQuantity;
+
+            $sql_insert = "INSERT INTO invoice (nama_pembeli, jumlah_tiket, jenis_tiket, nama_konser, user_id, datakonser_id, no_tlp, user_email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmt_insert = $conn->prepare($sql_insert);
+            $stmt_insert->bind_param("siisiiis", $namaPemesan, $selectedQuantity, $selectedTicket, $concertName, $_SESSION['user_id'], $datakonserId, $noTlpPemesan, $emailPemesan);
+            $stmt_insert->execute();
+            $stmt_insert->close();
+
+            // Generate invoice for the user
+            $invoiceMessage = "Thank you for your booking!\n\n";
+            $invoiceMessage .= "Concert: " . $concertName . "\n";
+            $invoiceMessage .= "Ticket Type: " . $selectedTicket . "\n";
+            $invoiceMessage .= "Quantity: " . $selectedQuantity . "\n";
+            $invoiceMessage .= "Unit Price: " . $harga . "\n";
+            $invoiceMessage .= "Total Price: " . $totalHarga . "\n";
+
+            // You can further customize the invoice message as needed
+
+            // Display invoice message to the user
+            echo "<script>alert('" . $invoiceMessage . "');</script>";
+            $errorMessage = ""; // Clear the error message after successful booking
         }
     } else {
-
+        // If the form is not submitted, initialize the error message
         $errorMessage = "";
     }
 } catch (mysqli_sql_exception $e) {
     echo "Error: " . $e->getMessage();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -74,7 +106,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AVH Tickets</title>
     <link rel="stylesheet" href="asset/style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" integrity="sha256-46r060N2LrChLLb5zowXQ72/iKKNiw/lAmygmHExk/o=" crossorigin="anonymous" />
 </head>
 <body>  
     <div id="navbar">
@@ -85,7 +117,7 @@ try {
                 <li><a href="index.php">Home</a></li>
                 <li><a href="ticket.php">Ticket</a></li>
                 <?php
-                    if (isset($_SESSION['email']) && $_SESSION['email'] === 'admin@gmail.com') {
+                    if (isset($_SESSION['user_email']) && $_SESSION['user_email'] === 'admin@gmail.com') {
                         echo '<li><a href="inputdata.php">Input</a></li>';
                     }
                 ?> 
@@ -96,6 +128,7 @@ try {
                         <button type="submit" id="searchb"><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
                     </form>
                 </li>
+                <!-- <li><a href="conf/logout.php" class="nav-link">Logout</a></li> -->
                 <div class="closeMenu"><i class="fa fa-times"></i></div>
                 <span class="icons">
                     <i class="fab fa-github"></i>
@@ -104,7 +137,7 @@ try {
                     <button onclick="myFunction()" class="dropbtn"><i class="fa-solid fa-user"></i></button>
                     <div id="myDropdown" class="dropdown-content">
                         <?php if ($isLoggedIn): ?>
-                            <li><a href="#" class="nav-link">Akun</a></li>
+                            <li><a href="#" class="nav-link"><?php echo $_GET['user_nama'];?></a></li>
                             <li><a href="#" class="nav-link">Riwayat</a></li>
                             <li><a href="conf/logout.php" class="nav-link">Logout</a></li>
                         <?php else: ?>
@@ -115,12 +148,9 @@ try {
             </ul>
         </nav>
     </div>
-    <div class="title">
-        <h1><?php if (isset($concertName)) echo $concertName; ?></h1>
-    </div>
     <div id="inputdata">
         <div class="form_input">
-            <form action="conf/log_pesan.php" method="post" enctype="multipart/form-data">
+            <form action="<?php echo $_SERVER['PHP_SELF'] . '?id=' . $concertId; ?>" method="post" enctype="multipart/form-data">
                 <table>
                 <tr>
                     <td><label for="nama_pemesan">Nama </label></td>
@@ -167,7 +197,7 @@ try {
             </form>
         </div>
     </div>
-    <script id="ticketData" type="application/json"><?php echo json_encode($ticketTypes); ?></script>
-    <script src="asset/js/script.js"></script>
 </body>
+<script src="https://kit.fontawesome.com/ef9e5793a4.js" crossorigin="anonymous"></script>
+<script src="asset/js/script.js"></script>
 </html>
